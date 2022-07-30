4fab98fa87e87b4e6c95195546b06112c8caf2eb
Paul C. Owe <40435982+owecodes@users.noreply.github.com>
2022-07-29 23:56:48 -0400

    status codes implementation

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ Author: Paul Owe
 Date: 29 July, 2022
 """
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status
 from enum import Enum
+from typing import Optional # In case you want to provide optional parameters for your API methods
 
 app = FastAPI()
 
@@ -14,9 +15,17 @@ def index():
 
     return {'message': 'Hello AI engineering world!'}
 
+# @app.get('/blog/all')
+# def get_all_blogs():
+#     return {'message':'All blogs fam'}
+
 @app.get('/blog/all')
-def get_all_blogs():
-    return {'message':'All blogs fam'}
+def get_all_blogs(page_size=2, page:Optional[int] = None):
+    return {'message':f'All {page_size} blogs on page {page}'}
+
+@app.get('/blog/{id}/comments/{comment_id}')
+def get_comment(id:int, comment_id:int, valid: bool = True, username: Optional[str]=None):
+    return {'message': f'blog_id {id}, comment_id {comment_id}, valid comment? {valid}, username {username}'}
 
 class BlogType(str, Enum):
    short='short-blog'
@@ -28,9 +37,14 @@ def get_blogs_by_type(type: BlogType):
     return {'message':f'Blogs of type {type}'} 
 
 
-@app.get('/blog/{id}')
-def get_blog(id:int):
-    return {'message':f'Blog with ID {id}'}
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK)
+def get_blog(id:int, response: Response):
+    if id> 5:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {'error': f'Blog id {id} not found'}
+    else: 
+        response.status_code = status.HTTP_200_OK 
+        return {'message':f'Blog with ID {id}'}
 
 
 
