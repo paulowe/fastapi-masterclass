1740692dd4774ea9afbe78cb53ae37ad0629f609
Paul C. Owe <40435982+owecodes@users.noreply.github.com>
2022-07-30 01:17:27 -0400

    tags and response descriptions

--- main.py
+++ main.py
@@ -19,12 +19,27 @@ def index():
 # def get_all_blogs():
 #     return {'message':'All blogs fam'}
 
-@app.get('/blog/all')
+@app.get(
+    '/blog/all',
+    tags=['blog'],
+    summary = "Gets all blogs",
+    description=" Moer detailde description",
+    response_description="the list of all blogs rd ")
 def get_all_blogs(page_size=2, page:Optional[int] = None):
     return {'message':f'All {page_size} blogs on page {page}'}
 
-@app.get('/blog/{id}/comments/{comment_id}')
+@app.get(
+    '/blog/{id}/comments/{comment_id}',
+    tags=['blog', 'comment'])
 def get_comment(id:int, comment_id:int, valid: bool = True, username: Optional[str]=None):
+    """
+     **id** - id of blog
+
+     valid - bool
+
+     uname - optional int 
+
+    """
     return {'message': f'blog_id {id}, comment_id {comment_id}, valid comment? {valid}, username {username}'}
 
 class BlogType(str, Enum):
@@ -32,12 +47,12 @@ class BlogType(str, Enum):
    story='story-blog'
    howto='howto-blog'
 
-@app.get('/blog/type/{type}')
+@app.get('/blog/type/{type}', tags=['blog'])
 def get_blogs_by_type(type: BlogType):
     return {'message':f'Blogs of type {type}'} 
 
 
-@app.get('/blog/{id}', status_code=status.HTTP_200_OK)
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK, tags=['blog'])
 def get_blog(id:int, response: Response):
     if id> 5:
         response.status_code = status.HTTP_404_NOT_FOUND
